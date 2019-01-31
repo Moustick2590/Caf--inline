f57bc1e4891447f89b0e16315239de10880a787b
gelelf <coralie.leclerc@outlook.com>
2019-01-31 15:01:04 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,38 @@
+<?php
 
+</div> <!-- site-content -->
+<footer class="container-fluid">
+    <div id="footer_container" class="container">
+    <nav class="navbar">
+  <a class="navbar-brand" href="#">CAFE INLINE <br>
+  ACCESS CODE SCHOOL</a>
+  
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">ACCUEIL <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ARTICLES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PROJETS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">LA TEAM</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">MENTIONS LEGALES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fab fa-linkedin"></i></a>
+        <a class="nav-link" href="#"><i class="fab fa-facebook-square"></i></a>
+        <a class="nav-link" href="#"><i class="fab fa-twitter-square"></i></a>
+      </li>
+    </ul>
+</nav>
+    </div>
 
-
-
+</footer>
 
 
 
