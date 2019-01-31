7ffa82fef102e87ed56eb35946ec19344ff28726
Moustick2590 <aurel.caillaud@gmail.com>
2019-01-31 13:39:52 +0100

    ajout des logos

--- header.php
+++ header.php
@@ -4,9 +4,56 @@
         <title>Café-inline</title>
         <meta name="description" content="Blog de la promotion Café-inline">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Café-inline" />
+        <meta name="twitter:description" content="Le site de la promo Access Code School 2018/2019" />
+        <meta name="twitter:image" content="images/icons/logo_café-inline_petit.png" />
+        <meta name="twitter:image:alt" content="logo Café-inline" /> 
+        <meta property="og:url" content="localhost/wordpress" />
+        <meta property="og:title" content="Café-inline" />
+        <meta property="og:description" content="Le site de la promo Access Code School 2018/2019" />
+        <meta property="og:image" content="images/icons/logo_café-inline_petit.png" />
+        <meta property="og:image:alt" content="logo Café-inline" /> 
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/font-awesome.min.css">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <header>
+            <div class="container-fluid" id="navBar">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg " id="scrollSpy">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                    <div class="collapse navbar-collapse justify-content-around" id="navbarTogglerDemo01">
+                        <a href="" class="navbar-brand"> <img src="img/logo-image.png" alt="logo restaurant" width="154" height="92"> </a>
+                        <ul class="navbar-nav mt-2 mt-lg-0 ">
+                            <li class="nav-item">
+                                <a class="nav-link lienNav " href="#promo">Home</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link lienNav " href="#about">About</a>
+                                <li class="nav-item ">
+                                    <a class="nav-link lienNav " href="#ingredients">Igredients</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link lienNav " href="#menu">Menu</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link lienNav " href="#reviews">Reviews</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link lienNav " href="#reservation">Reservations</a>
+                                </li>
+                                <li class="nav-item lienExt">
+                                    <a class="nav-link lienNav" href="#"> <span class="iconeNavbar" id="twitter1"> </span> </a>
+                                    <a class="nav-link lienNav" href="#"> <span class="iconeNavbar" id="youtube1"> </span> </a>
+                                    <a class="nav-link lienNav" href="#"> <span class="iconeNavbar" id="facebook1"> </span> </a>
+                                </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
