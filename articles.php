0bd0f4cc91f45915a32d8e17da282d89ba926fac
elmaestro84 <edouard.liegeon@gmail.com>
2019-02-04 16:27:58 +0100

    articles

--- articles.php
+++ articles.php
@@ -1,5 +1,5 @@
 <div class="post-list">
-<?php $myposts = get_posts( 'numberposts=-1&offset=$debut' );
+<?php $myposts = get_posts('numberposts=-1&offset=$debut');
 foreach( $myposts as $post) : setup_postdata( $post ) ?>
 <h1><?php the_title(); ?></h1>
 <?php echo get_the_post_thumbnail(); ?>
