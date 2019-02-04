47cb77c8d3b3f1072a216daeda441ab0006945d8
sowaarrior <soavolorys@gmail.com>
2019-02-04 12:00:19 +0100

    sandra

--- single.php
+++ single.php
@@ -1,63 +1,57 @@
 <?php get_header(); ?>
-<section id="article">
-<div class="container-fluid">
- <div>   
-    <div class="row">
-        <div class="col-md-5 offset-md-1">
 
+<div id="article" class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
             <?php
         if( have_posts() ): 
             while( have_posts() ): the_post(); ?>
-            <h3>
+
+            <h2 id="posttitle">
                 <?php the_title(); ?>
-                <h3>
-                    <p>
-                        <?php the_content(); ?>
-                    </p>
-                    <?php endwhile;
+            </h2>
+        </div>
+
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+
+
+            <p>
+                <?php the_content(); ?>
+            </p>
+            <?php endwhile;
         endif;
         ?>
-    
-        
-        <?php 
-       the_author();
-        ?> 
-         </div>
-        <div>
-        <?php 
-      echo get_the_date();
-        ?> 
+
+
+            <p id="postedby">Posté par
+                <?php 
+       the_author(); 
+                    ?>,
+                le
+                <?php
+       echo get_the_date();
+        ?>
+            </p>
+
         </div>
-        </div>
-        <div class="col-md-4 offset-md-1">
+        <div id="rightblock" class="col-md-4  offset-md-1">
+            <p id="recentspost">Articles récents</p>
             <ul>
-        <?php
+                <?php
 	$recent_posts = wp_get_recent_posts();
 	foreach( $recent_posts as $recent ){
 		echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
 	}
     wp_reset_query();
 ?>
-        </ul>
-                    <?php comments_template(); ?>  
+            </ul>
+            <?php comments_template(); ?>
         </div>
     </div>
-</section>
+</div>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
