ee42cca97921c22c088ac23ef534df9cae44be07
Moustick2590 <aurel.caillaud@gmail.com>
2019-02-05 08:38:07 +0100

    récupération

--- articles.php
+++ articles.php
@@ -1,12 +1,25 @@
-<div class="post-list">
-<?php $myposts = get_posts('numberposts=-1&offset=$debut');
-foreach( $myposts as $post) : setup_postdata( $post ) ?>
-<h1><?php the_title(); ?></h1>
-<?php echo get_the_post_thumbnail(); ?>
+<?php
+/* Template Name: Articles
+*/
+?>
+<?php get_header(); ?>
+<?php get_template_part( 'template-parts/template-part', 'mainheader' ); ?>
 
-<!-- Only display part of the post so the user has to click "More!" -->
-<?php the_excerpt(); ?>
+<main>
+    <section class="container-fluid">
+        <div class="container">
+            <div class="post-list">
+                <?php $myposts = get_posts('numberposts=-1&offset=$debut');
+                foreach( $myposts as $post) : setup_postdata( $post ) ?>
+                <h1><?php the_title(); ?></h1>
+                <?php echo get_the_post_thumbnail(); ?>
 
-<a href="<?php the_permalink(); ?>">Voir plus</a>
-<?php endforeach; ?>
-</div>
+                <!-- Only display part of the post so the user has to click "More!" -->
+                <?php the_excerpt(); ?>
+
+                <a href="<?php the_permalink(); ?>">Voir plus</a>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </section>
+</main>
